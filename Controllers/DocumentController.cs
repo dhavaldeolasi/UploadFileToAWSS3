71e870a6c9a9fff2f1038c26dd5858a0dbc5a911
Dhaval Deolasi <dhaval.deolasi@gmail.com>
2020-07-19 11:03:56 +0530

    First Commit

---
+++ Controllers/DocumentController.cs
@@ -0,0 +1,66 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using AWSS3FileUpload.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AWSS3FileUpload.Controllers
+{
+    [Route("api/Document")]
+    [ApiController]
+    public class DocumentController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult> UploadDocument(IFormFile document)
+        {
+
+            bool isUploadSuccess = false;
+            if (document != null)
+            {
+                Stream stream = document.OpenReadStream();
+                DocumentService documentService = new DocumentService();
+                isUploadSuccess = await documentService.UploadDocument(stream, document.FileName);
+                await stream.DisposeAsync();
+            }
+            else
+            {
+                return BadRequest("Please select file to upload");
+            }
+
+            if (isUploadSuccess)
+            {
+                return Ok("Document uploaded successfully");
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = "Error occurred" });
+            }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> DownloadDocument(string fileName)
+        {
+
+            bool isDownloadSuccess = false;
+            if (!string.IsNullOrEmpty(fileName))
+            {
+                DocumentService documentService = new DocumentService();
+                isDownloadSuccess = await documentService.DownloadDocument(fileName);
+            }
+            else
+            {
+                return BadRequest("Please provide the file name");
+            }
+
+            if (isDownloadSuccess)
+            {
+                return Ok("Document downloaded successfully in D:\\AWSDocuments");
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = "Error occurred" });
+            }
+        }
+
+    }
+}
---
+++ Services/DocumentService.cs
@@ -0,0 +1,82 @@
+﻿using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.S3.Transfer;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AWSS3FileUpload.Services
+{
+    public class DocumentService
+    {
+        private const string bucketName = "*** provide bucket name ***";
+        private const string accessKey = "*** provide access key ***";
+        private const string secretKey = "*** provide secret key ***";
+
+        // Specify your bucket region here
+        private static readonly RegionEndpoint bucketRegion = RegionEndpoint.APSouth1;
+        private static IAmazonS3 s3Client;
+
+        public async Task<bool> UploadDocument(Stream stream, string fileName)
+        {
+            try
+            {
+                s3Client = new AmazonS3Client(accessKey, secretKey, bucketRegion);
+                TransferUtility transferUtility = new TransferUtility(s3Client);
+                //Set partSize to 5 MB
+                long partSize = 5 * 1024 * 1024;
+                TransferUtilityUploadRequest transferUtilityUploadRequest = new TransferUtilityUploadRequest
+                {
+                    BucketName = bucketName,
+                    InputStream = stream,
+                    StorageClass = S3StorageClass.Standard,
+                    CannedACL = S3CannedACL.Private,
+                    PartSize = partSize, 
+                    Key = fileName
+                };
+
+                await transferUtility.UploadAsync(transferUtilityUploadRequest);
+                transferUtility.Dispose();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //Log exception
+                return false;
+            }
+        }
+
+        public async Task<bool> DownloadDocument(string fileName)
+        {
+            try
+            {
+                string directoryPath = @"D:\AWSDocuments\";
+                if (!Directory.Exists(directoryPath))
+                {
+                    Directory.CreateDirectory(directoryPath);
+                }
+                s3Client = new AmazonS3Client(accessKey, secretKey, bucketRegion);
+                GetObjectRequest request = new GetObjectRequest
+                {
+                    BucketName = bucketName,
+                    Key = fileName,
+                };
+
+                using (GetObjectResponse response = await s3Client.GetObjectAsync(request))
+                {
+                    string filePath = directoryPath + fileName;
+                    await response.WriteResponseStreamToFileAsync(filePath, false, CancellationToken.None);
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                //Log exception
+            }
+            return false;
+        }
+
+    }
+}
